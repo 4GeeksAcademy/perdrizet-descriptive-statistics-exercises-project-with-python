8f44de286bd0ae9c0ef92e7bfae537517d5e04c0
George Perdrizet <george@perdrizet.org>
2025-07-19 00:13:23 +0000

    Moved test to test directory

--- quick_test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from functions import add_title_length_features
-
-# Create a test dataframe
-test_df = pd.DataFrame({'title': ['Test', 'Another Test', 'A Much Longer Title']})
-result = add_title_length_features(test_df)
-print('Test successful!')
-print(result)
